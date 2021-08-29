21271d63dfbbfa61f525519b5b1830507de12f9d
laukintung322@gmail.com <laukintung322@gmail.com>
2021-08-29 13:29:33 +0800

    Initial commit

---
+++ src/GlobalStyle.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+:root{
+    --darkGrey: #1c1c1c;
+}
+
+body{
+    margin: 0;
+    padding: 0;
+}
+
+`
