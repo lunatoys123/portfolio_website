0b1019099ca9ff1658171228eb21f85ad3fe32c8
tktlau <laukintung322@gmail.com>
2021-08-30 16:07:04 +0800

    Add Content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./Components/navbar";
-import Header from "./Components/Header"
+import Header from "./Components/Header";
+import Content from "./Components/MainContent";
 function App() {
   return (
     <>
       <Navbar />
       <Header />
+      <Content />
     </>
   );
 }
