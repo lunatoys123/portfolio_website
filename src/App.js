34a317d8bde55de1d4983d49526851871b46c0f2
tktlau <laukintung322@gmail.com>
2021-08-30 14:01:06 +0800

    restart

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react'
-import Header from './Components/Header'
-import HomePage from './Components/HomePage'
-import Footer from './Components/Footer'
-import {GlobalStyle} from './GlobalStyle'
+
 function App() {
 
   return (
     <>
-      <Header/>
-      <HomePage/>
-      <Footer/>
-      <GlobalStyle />
+        MainPage
     </>
   );
 }
