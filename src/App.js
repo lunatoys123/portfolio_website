2831d5070bcaa41f04bd117ff5b5a5f10b564958
tktlau <laukintung322@gmail.com>
2021-08-30 15:51:22 +0800

    add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import Navbar from './Components/navbar'
+import React from "react";
+import Navbar from "./Components/navbar";
+import Header from "./Components/Header"
 function App() {
-
   return (
     <>
-        <Navbar />
+      <Navbar />
+      <Header />
     </>
   );
 }
