236bf519f6009a821dc346ddfb0949337f058ccc
laukintung322@gmail.com <laukintung322@gmail.com>
2021-09-22 15:55:13 +0800

    workproject css

---
+++ src/Components/workProjects/workProject.style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const BackGroundLayout = styled.div`
+    height: 100%;
+    min-height: 1000px;
+    width: 80%;
+    margin-left: auto;
+    margin-right: auto;
+    padding-top:20px
+`;
