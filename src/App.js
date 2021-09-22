d7a2e5aecb888bc5d08712b4499fb48b360bdff7
laukintung322@gmail.com <laukintung322@gmail.com>
2021-09-22 15:54:18 +0800

    add workProject to route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Home from './Components/Home'
 import Learning from './Components/Learning'
 import { GlobalStyle } from "./GlobalStyle";
+import Project from "./Components/workProject"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/Learning/:year" component={Learning} />
+        <Route exact path="/workProject" component={Project} />
         </Switch>
       </Router>
     </>
