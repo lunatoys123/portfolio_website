0a40cfb6c1533673c372488f302fa8bbb3ff9c43
laukintung322@gmail.com <laukintung322@gmail.com>
2021-09-22 16:05:21 +0800

    workProject page initialized

---
+++ src/Components/workProject.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./navbar";
+import Footer from "./Footer";
+import Submenu from "./submenu";
+import Project from "./workProjects";
+const WorkProject = () => {
+  return (
+    <>
+      <Navbar />
+      <Project />
+      <Footer />
+      <Submenu />
+    </>
+  );
+};
+
+export default WorkProject;
