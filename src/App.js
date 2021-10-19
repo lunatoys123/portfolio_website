2f0d0a9fd6c5304b8e038f445074fcad1d25370f
laukintung322@gmail.com <laukintung322@gmail.com>
2021-10-19 18:30:21 +0800

    add redirect route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { lazy, Suspense } from "react";
 import { GlobalStyle } from "./GlobalStyle";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
 const Home = lazy(() => import("./Components/Home"));
 const Learning = lazy(() => import("./Components/Learning"));
@@ -15,6 +15,7 @@ function App() {
             <Route exact path="/" component={Home} />
             <Route exact path="/Learning/:year" component={Learning} />
             <Route exact path="/workProject" component={Project} />
+            <Redirect from={"/Learning"} to={`/Learning/Year1`}/>
           </Switch>
         </Suspense>
       </Router>
