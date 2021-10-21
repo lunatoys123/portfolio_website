c090f164c99bc1472935c9fdb3da0f21b5a6414d
laukintung322@gmail.com <laukintung322@gmail.com>
2021-10-21 20:59:06 +0800

    update individual project

--- src/Components/workProjects/indvidualProject.js
+++ src/Components/workProjects/indvidualProject.js
@@ -16,21 +16,21 @@ const Footer = lazy(() => import("../Footer"));
 const IndvidualProject = () => {
   const location = useLocation();
   const { title } = useParams();
-  const[ProjectPanelTitle, setProjectPanelTitle] =useState([]);
+  const [ProjectPanelTitle, setProjectPanelTitle] = useState([]);
 
   useEffect(() => {
     setProjectPanelTitle([]);
     if (location.state == null) {
-      ProjectData.map((item, index)=>{
-        const {Title} = item;
-        return setProjectPanelTitle((ProjectTitle)=>[...ProjectTitle, Title]);
-      })
+      ProjectData.map((item) => {
+        const { Title } = item;
+        return setProjectPanelTitle((ProjectTitle) => [...ProjectTitle, Title]);
+      });
     } else {
       console.log(location.state);
-      location.state.map((item, index)=>{
-        const {Title} = item;
-        return setProjectPanelTitle((ProjectTitle)=>[...ProjectTitle, Title]);
-      })
+      location.state.map((item) => {
+        const { Title } = item;
+        return setProjectPanelTitle((ProjectTitle) => [...ProjectTitle, Title]);
+      });
     }
   }, [location, title]);
 
@@ -51,17 +51,17 @@ const IndvidualProject = () => {
       <Container
         fluid
         style={{
-          maxwidth: "1280px",
+          maxWidth: "100%",
           minHeight: "100vh",
         }}
       >
         <Row>
           <Col
             sm={2}
-            style={{ borderRight: "1px solid #ffcccc", height: "100vh" }}
+            style={{ borderRight: "1px solid #ffcccc", minHeight: "100vh" }}
           >
-            {ProjectPanelTitle.map((item, index)=>{
-              return <p key={index}>{item}</p>
+            {ProjectPanelTitle.map((item, index) => {
+              return <p key={index}>{item}</p>;
             })}
           </Col>
           <Col sm={10}>
@@ -89,9 +89,8 @@ const IndvidualProject = () => {
                   <Image
                     src={graph}
                     style={{
-                      width: "80%",
-                      height: "70%",
                       border: "1px solid black",
+                      width: "80%",
                     }}
                   />
                 </Col>
@@ -157,7 +156,24 @@ const IndvidualProject = () => {
                                 logout
                               </ListGroup.Item>
                               <ListGroup.Item>
-                                
+                                Restaurant Controller responsible for handling
+                                actions when user interact with the coupon in
+                                the coupon system. for example, Create coupon,
+                                update coupon, show coupon, searching coupon,
+                                delete coupon, add coupon to user, show the
+                                users who purchase the coupon etc.
+                              </ListGroup.Item>
+                            </ListGroup>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>View</td>
+                          <td>
+                            <ListGroup>
+                              <ListGroup.Item>
+                                View use to handle to the display of the
+                                webpage, View can display the response data
+                                where Controller pass in
                               </ListGroup.Item>
                             </ListGroup>
                           </td>
@@ -165,6 +181,26 @@ const IndvidualProject = () => {
                       </tbody>
                     </Table>
                   </Row>
+                </Col>
+              </Row>
+              <Row>
+                <ColoredLine color="red" />
+              </Row>
+              <Row className="justify-content-md-center">
+                <h2 style={{ textAlign: "center" }}>Page Introduction</h2>
+                <Col
+                  sm={6}
+                  style={{
+                    alignItems: "center",
+                    display: "flex",
+                    justifyContent: "center",
+                    borderRight: "1px solid #ffcccc",
+                  }}
+                >
+                  Page picture
+                </Col>
+                <Col sm={6}>
+                  Page details
                 </Col>
               </Row>
             </Row>
